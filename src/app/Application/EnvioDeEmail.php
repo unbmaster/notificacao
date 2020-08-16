937b7cf66d9cddd5a4a9ea9f559fcfb536a3296a
UnBMaster <unbmaster@outlook.com>
2020-08-15 21:02:28 -0300

    Update 1

--- src/app/Application/EnvioDeEmail.php
+++ src/app/Application/EnvioDeEmail.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the https://github.com/unbmaster
  * For demonstration purposes, use it at your own risk.
- * (c) UnBMaster <unbmaster@outlook.com>
+ * (c) UnBMaster <unbmaster@outlook.com> 
  * License GNU General Public License (GPL)
  */
 
